e45a939a7afb9f6acf6e55b3cc7a63855a187121
prudhvi <prudhvisai3772@gmail.com>
2024-12-17 10:01:35 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import authRoutes from "./routes/authroutes.js";
+import connectDb from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send("Home");
+})
+
+app.use('/api/auth',authRoutes);
+
+const port = process.env.PORT || 8000
+app.listen(port,()=>{
+    console.log("listening...");
+    connectDb();
+});
